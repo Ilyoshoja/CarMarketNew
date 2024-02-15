3adb9bffe94bb4914a5f9f5cfba33928fe90a8a8
Ilyoshoja <imamashukurov@gmail.com>
2024-02-15 23:25:32 +0500

    DB va CarResource bajarilyabdi

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,4 +1,104 @@
 package db;
 
+import enums.UserRole;
+import bean.CarBean;
+import bean.UserBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DB {
+    public static UserBean session=null;
+    private static final List<UserBean> USER_BEANS =new ArrayList<>();
+    private static final List<CarBean> CAR_BEANS =new ArrayList<>();
+
+    private static int userID = 1;
+    private static int carID = 1;
+
+    static {
+        UserBean userBean = new UserBean();
+        userBean.setId(userID++);
+        userBean.setUsername("admin");
+        userBean.setPassword("admin");
+        userBean.setRole(UserRole.ADMIN);
+        USER_BEANS.add(userBean);
+    }
+    public static boolean checkUserExistByLogin(String login){
+        for (UserBean user : USER_BEANS) {
+            if (user.getUsername().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static UserBean addUser(UserBean userBean){
+        if(checkUserExistByLogin(userBean.getUsername())) {
+            return null;
+        }
+        userBean.setId(userID++);
+        userBean.setRole(UserRole.USER);
+        USER_BEANS.add(userBean);
+        userBean.setBalance(1000d);
+        return userBean;
+    }
+
+    public static UserBean getUser(UserBean userBean){
+        for (UserBean user : USER_BEANS) {
+            if (user.getUsername().equals(userBean.getUsername())&&user.getPassword().equals(userBean.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static CarBean addCar(CarBean carBean){
+        carBean.setId(carID++);
+        CAR_BEANS.add(carBean);
+        return carBean;
+    }
+
+    public static List<CarBean> getMyCars(Integer ownerId){
+        List<CarBean> myCars = new ArrayList<>();
+        for (CarBean carBean : CAR_BEANS) {
+            if (carBean.getUserId() != null && carBean.getUserId().equals(ownerId) && !carBean.getStore()){
+                myCars.add(carBean);
+            }
+        }
+        return myCars;
+    }
+
+    public static List<CarBean> getAvailableCars(){
+        List<CarBean> availableCars = new ArrayList<>();
+        for (CarBean carBean : CAR_BEANS) {
+            if (carBean.getUserId() == null && carBean.getStore()){
+                availableCars.add(carBean);
+            }
+        }
+        return availableCars;
+
+    }
+
+    public static List<CarBean> showAllCars(){
+        return CAR_BEANS;
+    }
+
+    public static boolean deleteCar(Integer carId){
+        CarBean car = getCar(carId);
+        if (car == null){
+            return false;
+        }
+        CAR_BEANS.remove(car);
+        return true;
+    }
+    public static CarBean getCar(Integer id){
+        for (CarBean carBean : CAR_BEANS) {
+            if (carBean.getId().equals(id)){
+                return carBean;
+            }
+        }
+        return null;
+    }
+
+
 }
