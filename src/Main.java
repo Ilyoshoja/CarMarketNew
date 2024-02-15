06328b4675c8e90df500d87e057ac6947646e3c9
Xusniddin <muxiddinovxusniddin34@gmail.com>
2024-02-15 22:50:30 +0500

    Xusniddin qo'shdi

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Salom test uchun");
         System.out.println("Xusniddin added new ");
+        System.out.println("Xusniddin added new ");
 
     }
 
